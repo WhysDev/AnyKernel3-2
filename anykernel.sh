f4ceaf1ae19c50b2f86f5670cd7d08e205d25c1b
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-08-19 23:29:55 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -22,7 +22,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
