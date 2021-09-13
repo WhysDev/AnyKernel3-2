05b91874aa88b8d20d41f4cc29b6825c61780c58
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-09-13 11:05:46 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SaranRom New Generation by @WhysDev @ xda-developers
+kernel.string=SaranRomKak Kernel by @WhysDev @ xda-developers
 do.devicecheck=1
 do.devicecheck=1
 do.modules=0
