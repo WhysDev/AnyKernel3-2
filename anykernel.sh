30edf29482bcda473226bb08e9855ea9e1007441
WhysDev <78435081+WhysDev@users.noreply.github.com>
2021-10-06 21:45:06 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Dark_Elixir_ryzen_SpecialEdition Kernel by @WhysDev @ xda-developers
+kernel.string=Super-Ryzen V3 by @WhysDev @ xda-developers
 do.devicecheck=1
 do.devicecheck=1
 do.modules=0
